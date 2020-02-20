a0be75b6373a87ddea4561bfae1d0379ecf90bd4
Karthick <karthick@rgmobiles.com>
2020-02-20 10:12:07 +0530

    New Functinalties

--- toast/src/main/java/com/toast/MyMessage.java
+++ toast/src/main/java/com/toast/MyMessage.java
@@ -1,6 +1,7 @@
 package com.toast;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyMessage
@@ -8,5 +9,10 @@ public class MyMessage
     public static void getMessage(String Message, Context context)
     {
         Toast.makeText(context,Message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void logAlert(String message)
+    {
+        Log.e("Android",message);
     }
 }
